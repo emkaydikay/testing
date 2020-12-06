5700c070504c0081d4cdf3e2c148bf47abfd7a20
root <root@localhost.localdomain>
2020-12-06 07:17:26 -0500

    This is 2nd commit from commit 2

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ print("This is second update from commit 1")
 print("This is third commit from commit 1")
 print("This is fourth commit from commit 1")
 print("This is first commit from commit2")
+print("This is 2nd commit from commit 2")
+
